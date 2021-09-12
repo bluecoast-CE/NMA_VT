361fd82c3503414a2944d5812011794eea3c3f8a
Bluecoast Consulting Engineers <55963218+bluecoast-CE@users.noreply.github.com>
2021-09-13 09:25:58 +1000

    1

---
+++ data/RTBAlignment_4.js
@@ -0,0 +1,15 @@
+var json_RTBAlignment_4 = {
+"type": "FeatureCollection",
+"name": "RTBAlignment_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Name": "S1", "Length": 25.0, "Orient": 141.0 }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 176.115367170923889, -5.676344478755073 ], [ 176.115187347149543, -5.67620777682418 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "S2", "Length": 25.0, "Orient": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 176.115567595443736, -5.676497524902854 ], [ 176.115751231025286, -5.676628433152974 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "S3", "Length": 25.0, "Orient": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 176.115948539132518, -5.676784688976215 ], [ 176.116127228427047, -5.676923120614545 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "N1", "Length": 25.0, "Orient": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 176.114158361822234, -5.674836676516664 ], [ 176.113992144797038, -5.674683666358418 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "N2", "Length": 25.0, "Orient": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 176.113806675127677, -5.674509903460032 ], [ 176.113649213557949, -5.674347866747804 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "GN1", "Length": 275.0, "Orient": null }, "geometry": { "type": "MultiLineString", "coordinates": [ ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "N3", "Length": 25.0, "Orient": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 176.11348018368065, -5.674162276294584 ], [ 176.113328663556075, -5.673994491491449 ] ] ] } },
+{ "type": "Feature", "properties": { "id": null, "Name": "N4", "Length": 25.0, "Orient": null }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 176.113146231712676, -5.673820406044746 ], [ 176.112986720573645, -5.673660393866172 ] ] ] } }
+]
+}
